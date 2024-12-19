5e5451f975b1b3b010c0de8842dbc23a11ee02a2
mrborghini <61702082+mrborghini@users.noreply.github.com>
2024-12-19 10:01:27 +0100

    Updated catgame

--- cat-game/index.service.worker.js
+++ cat-game/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1734189972|3400195025';
+const CACHE_VERSION = '1734598605|544707865';
 /** @type {string} */
 const CACHE_PREFIX = 'cat-game-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
