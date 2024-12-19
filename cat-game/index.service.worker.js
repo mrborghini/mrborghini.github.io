c8a1f2f3bbfe05d00d207080441eeee33fd46da4
mrborghini <61702082+mrborghini@users.noreply.github.com>
2024-12-19 15:01:19 +0100

    feat: fixed bsod

--- cat-game/index.service.worker.js
+++ cat-game/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1734604479|2550990267';
+const CACHE_VERSION = '1734616797|4923212109';
 /** @type {string} */
 const CACHE_PREFIX = 'cat-game-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
