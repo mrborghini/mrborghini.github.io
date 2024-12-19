197a3a005d6d68b814ca4c6252ac3159bf6d4d76
mrborghini <61702082+mrborghini@users.noreply.github.com>
2024-12-19 11:37:33 +0100

    feat: consistent boss spawns

--- cat-game/index.service.worker.js
+++ cat-game/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1734598605|544707865';
+const CACHE_VERSION = '1734604479|2550990267';
 /** @type {string} */
 const CACHE_PREFIX = 'cat-game-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
